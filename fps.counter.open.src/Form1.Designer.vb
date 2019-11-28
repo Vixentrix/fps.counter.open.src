6cbcc3c26725e1d99ea86ac0293703acd9398cce
Vixentrix <cyproxine@gmail.com>
2019-11-29 02:52:14 +0800

    Add files via upload

---
+++ fps.counter.open.src/Form1.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tmrFPSCounter = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrDrawFPSCounter = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'tmrFPSCounter
+        '
+        Me.tmrFPSCounter.Enabled = True
+        Me.tmrFPSCounter.Interval = 1
+        '
+        'tmrDrawFPSCounter
+        '
+        Me.tmrDrawFPSCounter.Enabled = True
+        Me.tmrDrawFPSCounter.Interval = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(1, Byte), Integer), CType(CType(1, Byte), Integer), CType(CType(10, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(261, 98)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "FPS Counter"
+        Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(1, Byte), Integer), CType(CType(1, Byte), Integer), CType(CType(10, Byte), Integer))
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tmrFPSCounter As Timer
+    Friend WithEvents tmrDrawFPSCounter As Timer
+End Class
---
+++ fps.counter.open.src/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Declare Function InvalidateRect Lib "user32" (ByVal hwnd As IntPtr, ByVal lpRect As IntPtr, ByVal bErase As Boolean) As Boolean
+
+    Private Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String) As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub tmrFPSCounter_Tick(sender As Object, e As EventArgs) Handles tmrFPSCounter.Tick
+        Try
+            If GetTickCount() - FPS_LastCheck >= 1000 Then
+                FPS_Current = FPS_Count
+                FPS_Count = 0 ' set the counter
+                FPS_LastCheck = GetTickCount()
+
+                ' highest FPS
+                If FPS_Current > FPS_Highest Then
+                    FPS_Highest = FPS_Current
+                End If
+            End If
+
+            FPS_Count = FPS_Count + 1
+        Catch ex As Exception
+            'dont msgbox otherwise if error occurs you'l get spammed to death.
+        End Try
+    End Sub
+
+    Private Sub tmrDrawFPSCounter_Tick(sender As Object, e As EventArgs) Handles tmrDrawFPSCounter.Tick
+        Try
+            Dim C1 As Graphics = Graphics.FromHwnd(New IntPtr(FindWindow("BlackDesert -", "BlackDesert -")))
+
+            Dim fontObj As Font
+            fontObj = New System.Drawing.Font("Tahoma", 12, FontStyle.Regular)
+
+            C1.DrawString(FPS_Current & " FPS", fontObj, Brushes.Lime, 10, 10)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
